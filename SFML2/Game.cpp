42d8194617826db3d9e403421bad8082bd0e02e9
Dominik Palenik <domuss574@gmail.com>
2019-08-16 12:41:07 +0200

    Podstawy
    Narazie jest tylko okno i petla input, update, render, wait ktora utrzymuje gre na 60 FPS

---
+++ SFML2/Game.cpp
@@ -0,0 +1,65 @@
+#include "Game.h"
+
+
+
+Game::Game()
+{
+	isRunning = true;
+	Window::window.create(sf::VideoMode(800, 600, 32), "Space Invaders");
+}
+
+void Game::exit()
+{
+	sf::Event event;
+	while (Window::window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+		{
+			Window::window.close();
+			isRunning = false;
+		}
+	}
+}
+
+void Game::update()
+{
+
+}
+
+void Game::output()
+{
+	Window::window.clear();
+	Window::window.display();
+}
+
+void Game::wait()
+{
+	static int FPS = 0;
+	static clock_t start = clock();
+	static int time;
+	static int zegar = clock();
+	static int delay = 1000 / maxFPS;
+
+	time = clock() - start;
+
+	if (delay > time)
+	{
+		Sleep(delay - time);
+	}
+	FPS++;
+
+	if ((clock() - zegar) > 1000)
+	{
+		std::cout << "FPS: " << FPS << std::endl;
+
+		FPS = 0;
+		zegar = clock();
+	}
+
+	start = clock();
+}
+
+Game::~Game()
+{
+
+}
---
+++ SFML2/Window.cpp
@@ -0,0 +1,3 @@
+#include "Window.h"
+
+sf::RenderWindow Window::window;
