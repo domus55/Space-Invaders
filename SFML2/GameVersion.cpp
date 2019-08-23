0ddf4cf7c335b5f3461257d5758ae56c24362bec
dev-Kamil <sackamacacacasaka@gmail.com>
2019-08-23 21:43:24 +0200

    Zmieniono pozycje wersji gry

--- SFML2/GameVersion.cpp
+++ SFML2/GameVersion.cpp
@@ -11,20 +11,15 @@ void GameVersion::create()
 	text2.setFont(font);
 
 	text.setFillColor(sf::Color::White);
-	text2.setFillColor(sf::Color::White);
-
-	/*sf::FloatRect textRect = text.getLocalBounds();
-	text.setOrigin(textRect.left + textRect.width / 2.0f,
-		textRect.top + textRect.height / 2.0f);*/
+	text2.setFillColor(sf::Color(255, 255, 255, 100));
 
 	text.setCharacterSize(10);
-	text.setPosition(700, 880);
+	text.setPosition(10, 880);
 	text.setLetterSpacing(10);
 	text.setString("SPACE INVADERS");
 
 	text2.setCharacterSize(10);
-	text2.setFillColor(sf::Color(255, 255, 255, 200));
-	text2.setPosition(10, 880);
+	text2.setPosition(10, 870);
 	text2.setString("version 0.1");
 }
 
