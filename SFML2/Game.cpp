25df4b4b36e29add8356a99bcfce46f1bd66f055
Dominik Palenik <domuss574@gmail.com>
2019-08-25 13:54:23 +0200

    Wielowatkowosc
    Ladowanie animacji i dzwiekow odbywa sie w watkach, dzieki czemu gra laduje sie ok. 7 razy szybciej

--- SFML2/Game.cpp
+++ SFML2/Game.cpp
@@ -6,9 +6,18 @@ Game::Game()
 	Window::window.create(sf::VideoMode(1600, 900, 32), "Space Invaders");
 	Window::window.clear(sf::Color(0, 0, 0));
 	Window::window.display();
-	Background::create();
-	LevelManager::create();
-	Explosion::loadAnimation();
+
+	std::thread t1(Background::create);
+	std::thread t2(LevelManager::create);
+	std::thread t3(Explosion::loadAnimation);
+	
+	t1.join();
+	t2.join();
+	t3.detach();
+
+	//Background::create();
+	//LevelManager::create();
+	//Explosion::loadAnimation();
 }
 
 void Game::exit()
